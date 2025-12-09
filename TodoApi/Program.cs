10c7125ba257f12908077612d32a75df79c91a59
ShoshanahBarzilai <shoshanahbarzilai@gmail.com>
2025-12-09 04:39:44 +0100

    ajout contexte base de données

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,20 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
+// <snippet_UseSwagger>
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
+// </snippet_UseSwagger>
 
 app.UseHttpsRedirection();
 
